ac4453bc5e938c52528a25f1894b9e3f1d69f6ee
caorushizi <84996057@qq.com>
2021-11-23 00:16:49 +0800

    编写脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,26 +4,28 @@
 ./qshell account "$1" "$2" blogfile
 
 # 上传文件并输出上传成功列表
+./qshell qupload2 --src-dir=./_site --bucket=zy-blogfile --check-exists --check-hash --thread-count=5 --overwrite --success-list=success.txt
+
 echo '==============================================='
 echo '=                success.txt                  ='
 echo '==============================================='
-./qshell qupload2 --src-dir=./_site --bucket=zy-blogfile --check-exists --check-hash --thread-count=5 --overwrite --success-list=success.txt
 cat success.txt
 
+awk '{printf "https://blog.ziying.site/%s\n", $2, $2}' success.txt | sed -e "s/index.html//" > refresh.txt
 
 echo '==============================================='
 echo '=                refresh.txt                  ='
 echo '==============================================='
-awk '{printf "https://blog.ziying.site/%s\n", $2, $2}' success.txt | sed -e "s/index.html//" > refresh.txt
 cat refresh.txt
 
 # 使用七牛云工具刷新CDN缓存
 ./qshell cdnrefresh -i refresh.txt
 
+awk '{printf "https://www.blog.ziying.site/%s\n", $2, $2}' success.txt | sed -e "s/index.html//" > refresh.www.txt
+
 echo '==============================================='
 echo '=              refresh.www.txt                ='
 echo '==============================================='
-awk '{printf "https://www.blog.ziying.site/%s\n", $2, $2}' success.txt | sed -e "s/index.html//" > refresh.www.txt
 cat refresh.www.txt
 
 # 使用七牛云工具刷新CDN缓存
