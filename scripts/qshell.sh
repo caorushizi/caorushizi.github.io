601622bba69368803a2b3dd6e1fe74499dac4955
caorushizi <84996057@qq.com>
2021-11-26 01:18:11 +0800

    feat: 修改部署脚本

--- scripts/qshell.sh
+++ scripts/qshell.sh
@@ -5,6 +5,6 @@ unzip qshell.zip -d tmp
 mv tmp/qsh* qshell
 chmod +x qshell
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_0.89.4_Linux-64bit.tar.gz hugo.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_0.89.4_Linux-64bit.tar.gz -O hugo.tar.gz
 tar -xzvf hugo.tar.gz
 chmod +x hugo
